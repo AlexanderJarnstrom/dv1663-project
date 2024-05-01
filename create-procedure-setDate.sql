f67d8562cd898fc0f09f8f7b0c22e2f7f0ac750d
AlexanderJarnstrom <alexander.jarnstrom@gmail.com>
2024-05-01 14:43:32 +0200

    Added simple error handling

--- create-procedure-setDate.sql
+++ create-procedure-setDate.sql
@@ -5,11 +5,13 @@ CREATE PROCEDURE SetDate(
   IN in_newDate DATE
 )
 BEGIN
-  IF curdate() < in_newDate THEN
-    UPDATE Borrows 
-    SET Borrows.EndDate = in_newDate
-    WHERE Borrows.BID = in_bid;
+  IF curdate() >= in_newDate THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Return date can\'t be in the past.';
   END IF;
+  
+  UPDATE Borrows 
+  SET Borrows.EndDate = in_newDate
+  WHERE Borrows.BID = in_bid;
 END $$
 
 DELIMITER ;
--- create-procedure-updateDate.sql
+++ create-procedure-updateDate.sql
@@ -1,20 +1,34 @@
+DROP PROCEDURE UpdateDate;
+
 DELIMITER $$
 
 CREATE PROCEDURE UpdateDate(
   IN in_bid INT,
-  IN in_months INT,
-  IN in_add BOOL
+  IN in_months INT
 )
 BEGIN
-  IF NOT in_add THEN
-    UPDATE Borrows
-    SET Borrows.EndDate = date_sub(Borrows.EndDate, INTERVAL in_months MONTH)
-    WHERE Borrows.BID = in_bid;
-  ELSE
-    UPDATE Borrows
-    SET Borrows.EndDate = date_add(Borrows.EndDate, INTERVAL in_months MONTH)
-    WHERE Borrows.BID = in_bid;
+  DECLARE borrow_exist INT;
+  DECLARE end_date DATE;
+  DECLARE new_date DATE;
+
+  SELECT Borrows.BID, Borrows.EndDate
+  INTO borrow_exist, end_date
+  FROM Borrows
+  WHERE Borrows.BID = in_bid;
+  
+  IF borrow_exist IS NULL THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'BID doesn\'t exist.';
   END IF;
+  
+  SET new_date = date_add(end_date, INTERVAL in_months MONTH);
+  
+  IF new_date < curdate() THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Return date can\'t be in the past.';
+  END IF;
+
+  UPDATE Borrows
+  SET Borrows.EndDate = new_date
+  WHERE Borrows.BID = in_bid;
 END $$
 
 DELIMITER ;
