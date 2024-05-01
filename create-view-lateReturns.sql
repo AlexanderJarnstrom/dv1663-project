f438f5dffa026018769fee2db1a8d687cc66652b
muaathaw <118123937+muaathaw@users.noreply.github.com>
2024-05-02 00:20:19 +0200

    Update create-view-lateReturns.sql

--- create-view-lateReturns.sql
+++ create-view-lateReturns.sql
@@ -2,3 +2,29 @@ CREATE VIEW LateReturns
 AS SELECT * 
 FROM Borrows
 WHERE Borrows.EndDate < curdate()
+--updated 
+////////
+CREATE VIEW LateReturns 
+AS 
+    SELECT
+        *,
+        CASE
+            WHEN DATEDIFF(curdate(), EndDate) <= 7 THEN 'Warning'
+            WHEN DATEDIFF(curdate(), EndDate) <= 14 THEN 'Late - Fine Imposed'
+            ELSE 'Very Late - Serious Action Required'
+        END AS LateStatus
+    FROM 
+        Borrows
+    WHERE 
+        EndDate < curdate();
+--OR
+CREATE VIEW LateReturns 
+AS 
+    SELECT 
+        *,
+        DATEDIFF(curdate(), EndDate) AS DaysLate
+    FROM 
+        Borrows
+    WHERE 
+        EndDate < curdate();
+
