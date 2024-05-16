cb1a616a671789a209adeaa42b32fd6862d91a51
Alexander <68053479+AlexanderJarnstrom@users.noreply.github.com>
2024-05-16 10:35:09 +0200

    Delete create-view-currentlyBorrowed.sql

--- create-view-currentlyBorrowed.sql
+++
@@ -1,8 +0,0 @@
-DROP VIEW IF EXISTS CurrentlyBorrowed;
-
-CREATE VIEW CurrentlyBorrowed 
-AS SELECT Borrows.ISBN, Books.Title, count(Borrows.ISBN) AS CurrentlyBorrowed, min(Borrows.EndDate) AS EarliestReturn 
-FROM Borrows 
-JOIN Books
-ON Borrows.ISBN = Books.ISBN
-GROUP BY Borrows.ISBN;
