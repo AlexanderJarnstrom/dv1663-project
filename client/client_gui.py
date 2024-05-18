418bdeeab3dc9e1a277ca95006352b8e6619395c
AlexanderJarnstrom <alexander.jarnstrom@gmail.com>
2024-05-18 14:51:42 +0200

    started on customer functions

--- client/client_gui.py
+++ client/client_gui.py
@@ -12,27 +12,34 @@ def home():
 @app.route("/books")
 def books():
     cont = get_books()
-    print(cont)
     return render_template("tables/book_table.html", books=cont)
 
 
 @app.route("/borrows")
 def currently_borrowed():
     content = get_currently_borrowed()
-    print(content)
     return render_template("tables/currently_borrow_table.html", content=content)
 
 
 @app.route("/customers")
 def customers():
     content = get_customers()
-    print(content)
+    return render_template("tables/customer_table.html", content=content)
+
+
+@app.route("/customers/add", methods = ["POST"])
+def customer_add():
+    return render_template("tables/customer_table.html")
+
+@app.route("/customers/search", methods = ["POST"])
+def customer_search():
+    cid = request.form["txt-search-cid"]
+    content = get_customers(cid = cid)
     return render_template("tables/customer_table.html", content=content)
 
 
 @app.route("/staff")
 def staff():
     content = get_staff()
-    print(content)
     return render_template("tables/staff_table.html", content=content)
 
